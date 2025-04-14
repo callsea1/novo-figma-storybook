7f141dd7223592c6354f8e8a42e45cb93ca6c661
Sean P. Callahan <callsea1@gmail.com>
2025-04-13 20:46:03 -0500

    Update Storybook configuration with TypeScript settings

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,30 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
     "@storybook/addon-onboarding",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+    "@storybook/addon-viewport",
+    "@storybook/addon-designs",
   ],
-  "framework": {
-    "name": "@storybook/react-webpack5",
-    "options": {}
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
   },
-  "staticDirs": [
-    "../public"
-  ]
+  docs: {
+    autodocs: "tag",
+  },
+  staticDirs: ["../public"],
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  output: {
+    path: "storybook-static",
+  },
 };
+
 export default config;
